e35f409e425a8d3161e3d8cb8bb34df42ad6075d
TTonlyV5 <514263710@qq.com>
2024-10-14 20:03:25 +0800

    Update InstanceState.cs

--- Modules/AI/AI.BPM/Core/Enums/InstanceState.cs
+++ Modules/AI/AI.BPM/Core/Enums/InstanceState.cs
@@ -12,40 +12,40 @@ namespace AI.BPM.Core.Enums
     /// </summary>
     public enum InstanceState
     {
-        [Description("未初始化")]
         /// <summary>
         /// 未初始化
         /// </summary>
-        UnInitiated =1<<7|DisplayType.Info, 
+        [Description("未初始化")]
+        UnInitiated = 1 | DisplayType.Info,
         /// <summary>
         /// 运行中
         /// </summary>
         [Description("运行中")]
-        Running = 2 << 7 | DisplayType.Primary,
+        Running = 2 | DisplayType.Primary,
         /// <summary>
         /// 已完成
         /// </summary>
         [Description("已完成")]
-        Finished = 3 << 7 | DisplayType.Success,
+        Finished = 3 | DisplayType.Success,
         /// <summary>
         /// 已取消
         /// </summary>
         [Description("已取消")]
-        Canceled = 4 << 7 | DisplayType.Warning,
+        Canceled = 4 | DisplayType.Warning,
         /// <summary>
         /// 流程异常
         /// </summary>
         [Description("异常")]
-        Exceptional=5<<7|DisplayType.Danger,
+        Exceptional = 5 | DisplayType.Danger,
         /// <summary>
         /// 流程挂起
         /// </summary>
         [Description("挂起")]
-        Suspended = 6 << 7 | DisplayType.Danger,
+        Suspended = 6 | DisplayType.Danger,
         /// <summary>
         /// 未知
         /// </summary>
         [Description("超时")]
-        Timeout = 7 << 7 | DisplayType.Danger 
+        Timeout = 7 | DisplayType.Danger
     }
 }
